61f69c58fc2909bd3abaae888e40469eaa08587b
Ravindra Lakal <ravindra.lakal@gmail.com>
2016-10-19 15:45:57 +0530

    updated postal code

--- glosel/glosel/doctype/post_code_cluster/post_code_cluster.js
+++ glosel/glosel/doctype/post_code_cluster/post_code_cluster.js
@@ -5,9 +5,16 @@
 cur_frm.add_fetch("post_code", "city", "city");
 cur_frm.add_fetch("post_code", "state", "state");
 cur_frm.add_fetch("post_code", "area", "area");
+cur_frm.add_fetch("sales_person", "employee", "employee");
 
 frappe.ui.form.on('Post Code Cluster', {
 	refresh: function(frm) {
 
+	},
+	validate: function(frm){
+		msgprint("hi");
+		for(i=0;i<frm.doc.sales_person_table.length;i++){
+			msgprint(frm.doc.sales_person_table[i].sales_person)
+		}
 	}
 });
