c08b4fbd04ffaa15d9b01d644183ae7542ad1ade
Ravindra Lakal <ravindra.lakal@gmail.com>
2016-10-20 13:12:12 +0530

    Updated cluster

--- glosel/glosel/doctype/post_code_cluster/post_code_cluster.js
+++ glosel/glosel/doctype/post_code_cluster/post_code_cluster.js
@@ -11,10 +11,32 @@ frappe.ui.form.on('Post Code Cluster', {
 	refresh: function(frm) {
 
 	},
-	validate: function(frm){
-		msgprint("hi");
-		for(i=0;i<frm.doc.sales_person_table.length;i++){
-			msgprint(frm.doc.sales_person_table[i].sales_person)
+	validate: function(frm, cdt,cdn){
+		if(frm.doc.sales_person_table){
+			for(i=0;i<frm.doc.sales_person_table.length;i++){
+			// msgprint(frm.doc.sales_person_table[i].sales_person)
+				abc = add_share(frm.doc.sales_person_table[i].employee,cdt,cdn)
+				b = JSON.parse(abc["responseText"])
+				frm.doc.sales_person_table[i].user_id = b.message.user_id; 
+			}
 		}
-	}
+	},
 });
+
+add_share = function(employee,cdt,cd){
+			return frappe.call({
+		       method: "frappe.client.get_value",
+		       async:false,
+		       args: {
+		           doctype: "Employee",
+		           fieldname: "user_id",
+		           filters: { name: employee },
+		       },
+		      	callback: function(res){
+		         	if (res && res.message){
+		         		 console.log(res.message['user_id']);
+		         		 // return res.message['user_id'];
+		          	}
+		      	}  	
+		   });
+}
---
+++ glosel/glosel/report/cluster_target_variance_item_group_wise/cluster_target_variance_item_group_wise.js
@@ -0,0 +1,33 @@
+// Copyright (c) 2016, New Indictrans Technologies PVT LTD and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Cluster Target Variance Item Group-Wise"] = {
+	"filters": [
+		{
+			fieldname: "fiscal_year",
+			label: __("Fiscal Year"),
+			fieldtype: "Link",
+			options: "Fiscal Year",
+			default: sys_defaults.fiscal_year
+		},
+		{
+			fieldname: "period",
+			label: __("Period"),
+			fieldtype: "Select",
+			options: [
+				{ "value": "Monthly", "label": __("Monthly") },
+				{ "value": "Quarterly", "label": __("Quarterly") },
+				{ "value": "Half-Yearly", "label": __("Half-Yearly") },
+				{ "value": "Yearly", "label": __("Yearly") }
+			],
+			default: "Monthly"
+		},
+		{
+			fieldname: "target_on",
+			label: __("Target On"),
+			fieldtype: "Select",
+			options: "Quantity\nAmount",
+			default: "Quantity"
+		},
+	]
+}
